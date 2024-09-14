1812c14f9af746ac39932430311db6dd38b7cee1
jaeyo03 <toyazx@naver.com>
2024-09-14 12:59:55 +0900

    solve : heap (다시 풀이)

--- Heap/programmers_42626.py
+++ Heap/programmers_42626.py
@@ -17,3 +17,28 @@ def solution(scoville, K):
             answer += 1
 
     return answer
+
+def solution_2(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+
+    while True:
+        if len(scoville) == 1:
+            s = heapq.heappop(scoville)
+            if s >= K:
+                break
+            else:
+                answer = -1
+                break
+        else:
+            s1 = heapq.heappop(scoville)
+            s2 = heapq.heappop(scoville)
+
+            if s1 >= K:
+                break
+            else:
+                new = s1 + 2 * s2
+                heapq.heappush(scoville, new)
+                answer += 1
+
+    return answer
