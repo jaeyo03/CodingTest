e0002f24a2352ece22d564ceac09723483906caa
jaeyo03 <toyazx@naver.com>
2025-03-18 00:45:30 +0900

    solve : dfs

--- DFS,BFS/beakjoon_14502.py
+++ DFS,BFS/beakjoon_14502.py
@@ -1,45 +1,53 @@
 import sys
 from itertools import combinations
+from copy import deepcopy
 
 n, m = map(int, sys.stdin.readline().split())
+graph = [list(map(int, sys.stdin.readline().split())) for _ in range(n)]
+answer = 0
 
-graph = []
-
-for _ in range(n):
-    graph.append(list(map(int, sys.stdin.readline().split())))
-
-def spread_virus(n,m,graph):
-    dy = [0,0,-1,1]
-    dx = [1,-1,0,0]
-    start_points = []
-    visited = [[False] * m] * n
+def spread_virus(n, m, graph):
+    dy = [0, 0, -1, 1]
+    dx = [1, -1, 0, 0]
+    stack = []
 
     for i in range(n):
         for j in range(m):
             if graph[i][j] == 2 :
-                start_points.append((i,j))
+                stack.append((i, j))
 
-    while start_points:
-        y,x = start_points.pop()
-        if not visited[y][x]:
-            visited[y][x] = True
-            for inx in range(4):
-                next_y = y + dy[inx]
-                next_x = x + dx[inx]
-                if 0 <= next_y < n and 0 <= next_x < m and graph[next_y][next_x] == 0:
-                    graph[next_y][next_x] = 2
-                    start_points.append((next_y,next_x))
-
+    while stack:
+        y, x = stack.pop()
+        for inx in range(4):
+            ny, nx = y + dy[inx], x + dx[inx]
+            if 0 <= ny < n and 0 <= nx < m and graph[ny][nx] == 0:
+                graph[ny][nx] = 2
+                stack.append((ny, nx))
     return graph
 
-for i in spread_virus(n,m,graph):
-    print(i)
-
-for possible_case in combinations(range(n * m), 3):
-    for point in possible_case:
-        y = point // m
-        x = point % m
-        if graph[y][x] == 2 or graph[y][x] == 1 :
+for walls in combinations(range(n * m), 3):
+    count = 0
+    temp_graph = deepcopy(graph)
+    is_valid = True
+    for wall in walls:
+        y, x = wall // m, wall % m
+        if temp_graph[y][x] != 0:
+            is_valid = False
             break
+        temp_graph[y][x] = 1
+    if not is_valid:
+        continue
 
-        graph[y][x] = 1
+    virus_spread_graph = spread_virus(n, m, temp_graph)
+
+    for i in range(n):
+        for j in range(m):
+            if virus_spread_graph[i][j] == 0:
+                count += 1
+
+    if count > answer:
+        answer = count
+
+    answer = max(answer, count)
+
+print(answer)
